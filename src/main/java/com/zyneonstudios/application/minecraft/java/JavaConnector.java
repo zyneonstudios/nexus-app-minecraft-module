aa37b7764c3837cb825bdddb84ce56ecb2750865
danieldieeins <daniel.niesmann@outlook.com>
2024-08-14 16:56:20 +0200

    Add RAM setting

--- src/main/java/com/zyneonstudios/application/minecraft/java/JavaConnector.java
+++ src/main/java/com/zyneonstudios/application/minecraft/java/JavaConnector.java
@@ -297,7 +297,9 @@ public class JavaConnector extends ModuleConnector {
                 OperatingSystemMXBean os = (OperatingSystemMXBean)ManagementFactory.getOperatingSystemMXBean();
                 long c = 1024L*1024L;
                 long max = os.getTotalMemorySize() /c;
-                frame.executeJavaScript("enableOverlay('file://"+ApplicationStorage.urlBase+ApplicationStorage.language+"/mje-memory.html?min=0&max="+max+"&value="+JavaStorage.memory+"');");
+                String url = ApplicationStorage.urlBase+ApplicationStorage.language+"/mje-memory.html?min=0&max="+max+"&value="+JavaStorage.memory;
+                System.out.println(url);
+                frame.executeJavaScript("enableOverlay('"+url+"');");
             } else if(request.startsWith("memory.")) {
                 try {
                     int i = Integer.parseInt(request.replace("memory.",""));
